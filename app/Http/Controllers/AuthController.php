8e5ecfdf08b7e108d9f52270932bbf37c799ce6e
Jeff Charles <krauklis046@gmail.com>
2023-12-09 17:27:38 +0200

    third commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Traits\HttpResponces;
+use App\Http\Requests\StoreUser;
 
 class AuthController extends Controller
 {
-    //
+    use HttpResponces;
+
+    public function login(){
+        return "This is my login Method";
+    }
+
+    public function register(StoreUser $request){
+
+        $request->validated($request->all());
+        $user = User::create([
+            'name' => $request->name,
+            'email'=>$request->email,
+            'password' => Hash::make($request->password),  
+        ]);
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('API Token of' . $user->name)->plainTextToken
+        ]);
+
+
+        return response()->json('This is my register Method');
+    }
+
+    public function logout(){
+        return response()->json('This is my logout Method');
+    }
 }
+
